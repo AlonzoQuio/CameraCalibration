0f4db1e96b99b5e033d27b5e989d227d1bce6d87
Raul <raul.romani@ucsp.edu.pe>
2018-02-02 19:04:07 -0500

    sobel

---
+++ sobel.cpp
@@ -0,0 +1,56 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace cv;
+
+/** @function main */
+int main( int argc, char** argv )
+{
+
+  Mat src, src_gray;
+  Mat grad;
+  int scale = 1;
+  int delta = 0;
+  int ddepth = CV_16S;
+
+  int c;
+
+  /// Load an image
+  src = imread( "img.png" );
+
+  if( !src.data )
+  { return -1; }
+
+  GaussianBlur( src, src, Size(3,3), 0, 0, BORDER_DEFAULT );
+
+  /// Convert it to gray
+  cvtColor( src, src_gray, CV_BGR2GRAY );
+
+  /// Create window
+  namedWindow( "Sobel Demo - Simple Edge Detector", CV_WINDOW_AUTOSIZE );
+
+  /// Generate grad_x and grad_y
+  Mat grad_x, grad_y;
+  Mat abs_grad_x, abs_grad_y;
+
+  /// Gradient X
+  //Scharr( src_gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
+  Sobel( src_gray, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
+  convertScaleAbs( grad_x, abs_grad_x );
+
+  /// Gradient Y
+  //Scharr( src_gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
+  Sobel( src_gray, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
+  convertScaleAbs( grad_y, abs_grad_y );
+
+  /// Total Gradient (approximate)
+  addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
+
+  imshow( window_name, grad );
+
+  waitKey(0);
+
+  return 0;
+  }
